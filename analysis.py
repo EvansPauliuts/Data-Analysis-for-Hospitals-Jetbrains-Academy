51207e18b5f789853ec6ea09ef8fa871f3170c9e
Evans Pauliuts <pauliutsevans@gmail.com>
2022-01-19 14:24:02 +0300

    Work on project. Stage 2/5: Merge them!

--- analysis.py
+++ analysis.py
@@ -1,4 +1,5 @@
 # write your code here
+import glob
 import pandas as pd
 
 
@@ -7,15 +8,19 @@ class AnalysisHospitals:
         pass
 
     @staticmethod
-    def data_all_csv(file_csv):
-        generals = pd.read_csv(file_csv)
-        print(generals.head(20))
+    def data_all_csv():
+        dataframes_list = [v for v in map(pd.read_csv, glob.glob('test/*.csv'))]
+        dataframes_list[1].rename(columns={'HOSPITAL': 'hospital', 'Sex': 'gender'}, inplace=True)
+        dataframes_list[2].rename(columns={'Hospital': 'hospital', 'Male/female': 'gender'}, inplace=True)
+
+        df = pd.concat(dataframes_list, ignore_index=True)
+        df.drop(columns=['Unnamed: 0'], inplace=True)
+
+        print(df.sample(n=20, random_state=30))
 
 
 def main():
-    csv_file_list = ['test/general.csv', 'test/prenatal.csv', 'test/sports.csv']
-    for v in csv_file_list:
-        AnalysisHospitals.data_all_csv(v)
+    AnalysisHospitals.data_all_csv()
 
 
 if __name__ == '__main__':
